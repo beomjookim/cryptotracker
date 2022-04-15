3cdad57f8dabedb7a2f5bb2bdf56e2b794f9298e
beomjookim <globalman96@naver.com>
2022-04-15 11:01:03 +0900

    Added routing pages
    Added homepage and coinpage in Page folder, and header in components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header'
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
 
 function App() {
   return (
-    <div className="App">HI</div>
+    <Router>
+      <div>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/coins/:id' element={<CoinPage/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
