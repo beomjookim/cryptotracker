a55926eef3831ba3d3600607a689a47876f7116f
beomjookim <globalman96@naver.com>
2022-04-16 00:06:35 +0900

    Implement whole header part except for select
    Select requires context API, this will be done tmw

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import Header from './components/Header'
 import Homepage from './Pages/Homepage';
 import CoinPage from './Pages/CoinPage';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/system';
+
+const StyledPaper = styled(Paper, {})({
+  backgroundColor: '#14161a',
+  color: 'white',
+  minHeight: '100vh'
+});
 
 function App() {
   return (
     <Router>
-      <div>
+      <StyledPaper>
         <Header/>
         <Routes>
           <Route path='/' element={<Homepage/>}/>
           <Route path='/coins/:id' element={<CoinPage/>}/>
         </Routes>
-      </div>
+      </StyledPaper>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import * as React from 'react';
+import {useNavigate} from 'react-router-dom';
+import {AppBar, Box, Toolbar, Typography, Select, InputLabel} from '@mui/material';
+import {MenuItem} from '@mui/material';
+import {styled} from '@mui/system';
+
+const StyledTypography = styled(Typography, {})({
+  flex: 1,
+  color: 'gold',
+  fontFamily: 'Montserrat',
+  fontWeight: 'bold',
+  cursor: 'pointer',
+});
+
+const StyledSelect = styled(Select, {})({
+  color: 'gold',
+  fontFamily: 'Montserrat',
+  cursor: 'pointer',
+  backgroundColor: '#3c434a',
+  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+    borderColor: 'gold',
+},
+});
 
 const Header = () => {
+  const navigate = useNavigate();
+  function handleClick(){
+    navigate("/");
+  }
+
   return (
-    <div>Header</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{bgcolor: "#1d2327"}}>
+        <Toolbar>
+          <StyledTypography onClick={handleClick} variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Crypto Tracker
+          </StyledTypography>
+          <InputLabel id="demo-simple-select-label" style={{color:"gold"}}>Currency</InputLabel>
+          <StyledSelect
+            variant="outlined"
+            style={{ width: 100, height: 40, marginLeft: 15}}
+            label="Currency"
+            // onChange={}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"KRW"}>KRW</MenuItem>
+            <MenuItem value={"GBP"}>GBP</MenuItem>
+          </StyledSelect>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
