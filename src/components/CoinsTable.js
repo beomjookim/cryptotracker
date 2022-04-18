88e4920afeaefb5d0048d2508afe5341b85aee52
beomjookim <globalman96@naver.com>
2022-04-18 23:35:43 +0900

    Implement loading component for better UX
    Improved UX by squeezing loading scene in before data is fully fetched

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -26,13 +26,13 @@ const StyledTableRow = styled(TableRow, {})({
     fontFamily: "Montserrat"
 })
 
-// const StyledPagination = styled(Pagination, {})({
-//     ul: {"& .MuiPaginationItem-root": {color: "gold"},
-//         '& .Mui-selected': {backgroundColor: 'gold', color: 'black'}},
-//     display: "flex",
-//     justifyContent: "center",
-//     padding: 20
-// })
+const StyledPagination = styled(Pagination, {})({
+    ul: {"& .MuiPaginationItem-root": {color: "gold"},
+        '& .Mui-selected': {backgroundColor: 'gold', color: 'black'}},
+    display: "flex",
+    justifyContent: "center",
+    padding: 20
+})
 
 const CoinsTable = () => {
     const navigate = useNavigate();
@@ -40,14 +40,14 @@ const CoinsTable = () => {
     const [search, setSearch] = useState("");
     const [page, setPage] = useState(1);
     const {currency, symbol} = CryptoState();
-    // const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         (async () => {
-        // setLoading(true);
+        setLoading(true);
         const {data} = await axios.get(CoinList(currency));
         setCoins(data);
-        // setLoading(false);
+        setLoading(false);
     })()}, [currency]);
 
     const handleSearch = () => {
@@ -68,70 +68,70 @@ const CoinsTable = () => {
                 onChange={(e) => {setSearch(e.target.value); console.log(e.target.value)}} />
                 <TableContainer>
                     {
-                        // loading?
-                        // <LinearProgress style={{background: 'gold'}}/>:
-                            <Table>
-                                <TableHead style={{backgroundColor: "white"}}>
-                                    <TableRow>
-                                        {["Coin", "Price", "24h Change", "Market Cap"].map((val) => (
-                                            <TableCell 
-                                            style={{color: "black", fontSize: 20, fontWeight: "bold", fontFamily: "Montserrat", backgroundColor: "gold"}}
-                                            key = {val}
-                                            align={val === "Coin"?"left":"right"}>
-                                                {val}
-                                            </TableCell>
-                                        ))}
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {handleSearch().slice((page-1)*10, page*10).map(coin => {
-                                        const plus = coin.price_change_percentage_24h >= 0;
+                        loading?
+                        <LinearProgress style={{background: 'gold'}}/>:
+                        <Table>
+                            <TableHead style={{backgroundColor: "white"}}>
+                                <TableRow>
+                                    {["Coin", "Price", "24h Change", "Market Cap"].map((val) => (
+                                        <TableCell 
+                                        style={{color: "black", fontSize: 20, fontWeight: "bold", fontFamily: "Montserrat", backgroundColor: "gold"}}
+                                        key = {val}
+                                        align={val === "Coin"?"left":"right"}>
+                                            {val}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {handleSearch().slice((page-1)*10, page*10).map(coin => {
+                                    const plus = coin.price_change_percentage_24h >= 0;
 
-                                        return (
-                                        <StyledTableRow
-                                        onClick={() => navigate(`.coins/${coin.id}`)}
-                                        key={coin.name}
-                                        >
-                                            <TableCell component="th" scopt="row"
-                                            style={{display: "flex", gap: 15}}>
-                                                <img
-                                                src={coin.image}
-                                                alt={coin.name}
-                                                height="50"
-                                                style={{marginBottom:10}}/>
-                                                <div>
-                                                    <span style={{
-                                                        textTransform: "uppercase",
-                                                        fontSize: 35,
-                                                        color: "white"
-                                                    }}>{coin.symbol}</span>{" "}
-                                                    <span style={{color: "wheat"}}>{coin.name}</span>
-                                                </div>
-                                            </TableCell>
-                                            <TableCell align="right" style={{color: "white", fontSize: 20}}>
-                                                {symbol}{" "}{coin.current_price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                                            </TableCell>
-                                            <TableCell
-                                            align="right"
-                                            style={{color: plus ? "green" : "red", fontSize: 18}}>
-                                                {plus && "+"}{coin.price_change_percentage_24h.toFixed(2)}%
-                                            </TableCell>
-                                            <TableCell align="right" style={{color:"white", fontSize: 17}}>
-                                                {symbol}{" "}{coin.market_cap.toString().slice(0, -6).replace(/\B(?=(\d{3})+(?!\d))/g, ',')} M
-                                            </TableCell>
-                                        </StyledTableRow>
-                                    )})}
-                                </TableBody>
-                            </Table>
+                                    return (
+                                    <StyledTableRow
+                                    onClick={() => navigate(`.coins/${coin.id}`)}
+                                    key={coin.name}
+                                    >
+                                        <TableCell component="th" scopt="row"
+                                        style={{display: "flex", gap: 15}}>
+                                            <img
+                                            src={coin.image}
+                                            alt={coin.name}
+                                            height="50"
+                                            style={{marginBottom:10}}/>
+                                            <div>
+                                                <span style={{
+                                                    textTransform: "uppercase",
+                                                    fontSize: 35,
+                                                    color: "white"
+                                                }}>{coin.symbol}</span>{" "}
+                                                <span style={{color: "wheat"}}>{coin.name}</span>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell align="right" style={{color: "white", fontSize: 20}}>
+                                            {symbol}{" "}{coin.current_price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                        </TableCell>
+                                        <TableCell
+                                        align="right"
+                                        style={{color: plus ? "green" : "red", fontSize: 18}}>
+                                            {plus && "+"}{coin.price_change_percentage_24h.toFixed(2)}%
+                                        </TableCell>
+                                        <TableCell align="right" style={{color:"white", fontSize: 17}}>
+                                            {symbol}{" "}{coin.market_cap.toString().slice(0, -6).replace(/\B(?=(\d{3})+(?!\d))/g, ',')} M
+                                        </TableCell>
+                                    </StyledTableRow>
+                                )})}
+                            </TableBody>
+                        </Table>
                     }
                 </TableContainer>
-                {/* <StyledPagination
+                <StyledPagination
                 count={parseInt((handleSearch()?.length/10).toFixed(0))}
                 onChange={(_, val) => {
                     setPage(val);
                     window.scroll(0, 450);
                 }}
-                page={page}/> */}
+                page={page}/>
             </Container>
         </div>
     )
