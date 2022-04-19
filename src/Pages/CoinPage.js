b4d875e2752c7932f37449c94d17cc2f361a827b
beomjookim <globalman96@naver.com>
2022-04-19 22:01:31 +0900

    Add more details in coin detail section
    added data such as rank, current price, markey cap at the bottom

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import { SingleCoin } from '../config/api';
 import { CryptoState } from '../CryptoContext';
 import CoinInfo from '../components/CoinInfo'
-import { Box, Typography } from '@mui/material';
+import { Box, LinearProgress, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import ReactHtmlParser from 'react-html-parser';
 
@@ -14,8 +14,8 @@ const StyledBox = styled(Box, {})({
   width:"30%", 
   alignItems:"center", 
   borderRight:"2px solid grey", 
-  marginTop: 20, 
-  padding:10
+  marginTop: 50, 
+  padding: 20
 })
 
 const CoinPage = () => {
@@ -29,20 +29,46 @@ const CoinPage = () => {
   useEffect(()=>{fetchCoin()}, []);
 
   return (
-    <Box sx={{display:"flex"}}>
+    coin?
+    (<Box sx={{display:"flex"}}>
       <StyledBox>
         <img src={coin?.image.large} alt={coin?.name} height="200" style={{marginBottom:20}}/>
-        <Typography variant="h3" sx={{fontWeight:"bold", marginBottom: 5, fontFamily:"Montserrat"}}>{coin?.name}</Typography>
+        <Typography variant="h3" sx={{
+          fontWeight:"bold", 
+          marginBottom: 5, 
+          fontFamily:"Montserrat"}}>
+            {coin?.name}
+        </Typography>
         <Typography variant="subtitle1" sx={{
           fontFamily:"Montserrat", 
           color:"grey", 
+          padding: 2,
+          textAlign:"center",
           "& > a":{color:"yellow", textDecoration:"none"}}}>
           {ReactHtmlParser(coin?.description.en.split('. ')[0])}
         </Typography>
+        <div>
+          <span sx={{display:"flex"}}>
+            <Typography variant='h5' sx={{marginTop: 5}}>
+              Rank:  {coin?.market_cap_rank}
+            </Typography>
+            <Typography variant='h5' sx={{marginTop: 2}}>
+              Current Price: {symbol} {coin?.market_data
+              .current_price[currency.toLowerCase()]
+              .toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </Typography>
+            <Typography variant='h5' sx={{marginTop: 2}}>
+              Market Cap: {symbol} {coin?.market_data
+              .market_cap[currency.toLowerCase()]
+              .toFixed(2).toString().slice(0,-6).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}M
+            </Typography>
+          </span>
+        </div>
       </StyledBox>
       <CoinInfo coin={coin} sx={{display:"flex", width:"60%"}}/>
     </Box>
-  )
-}
+  ):
+  <LinearProgress style={{backgroundColor: "gold"}}/>
+)}
 
 export default CoinPage
